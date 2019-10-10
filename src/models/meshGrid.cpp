923e4b0daf2bb83706a168bb48796e649d7452ce
maxime.isnel@etu.umontpellier.fr <isnel.maxime@gmail.com>
2019-10-10 09:11:07 +0200

    keep coding meshGrid Class

--- src/models/meshGrid.cpp
+++ src/models/meshGrid.cpp
@@ -1,1 +1,122 @@
 #include "meshGrid.h"
+
+#include <imgui.h>
+
+
+using namespace glm;
+
+MeshGrid::MeshGrid(unsigned int size, float w, float z){
+	nbPointPerRowColumn = size;
+	width = w;
+	gridZ = z;
+
+	nb_vertices = 0;
+	nb_faces = 0;
+
+	vertices = NULL;
+    normals  = NULL;
+    tangents = NULL;
+    colors   = NULL;
+    coords   = NULL;
+    faces    = NULL;
+
+    createMesh(nbPointPerRowColumn, width, gridZ);
+}
+
+
+MeshGrid::~MeshGrid(){
+
+}
+
+void MeshGrid::recreate(){
+	cleanup();
+	createMesh(nbPointPerRowColumn, width, gridZ);
+}
+
+
+
+void MeshGrid::createMesh(unsigned int size, float w, float z){
+
+	const float startingWidth = -(width/2.0f);
+
+	nb_vertices = size*size;
+	nb_faces = (size-1)*(size-1)*2;
+
+	vertices = (float *)malloc(3*nb_vertices*sizeof(float));
+    normals  = (float *)malloc(3*nb_vertices*sizeof(float));
+    tangents = (float *)malloc(3*nb_vertices*sizeof(float));
+    colors   = (float *)malloc(3*nb_vertices*sizeof(float));
+    coords   = (float *)malloc(2*nb_vertices*sizeof(float));
+    faces    = (unsigned int *)malloc(3*nb_faces*sizeof(unsigned int));
+
+    // points creation
+    for(unsigned int i=0; i<size; i++){
+    	for(unsigned int j=0; j<size; j++){
+
+    	}
+    }
+
+
+
+
+    computeCenter();
+    computeRadius();
+
+}
+
+
+void MeshGrid::addVertex(unsigned int arrayPos, vec3 pos, vec3 n, vec3 tangent, vec3 col, vec2 uv){
+	// position
+	vertices[3*arrayPos] = pos.x;
+	vertices[3*arrayPos+1] = pos.y;
+	vertices[3*arrayPos+2] = pos.z;
+	// normal
+	normals[3*arrayPos] = n.x;
+	normals[3*arrayPos+1] = n.y;
+	normals[3*arrayPos+2] = n.z;
+	// tangent
+	tangents[3*arrayPos] = tangent.x;
+	tangents[3*arrayPos+1] = tangent.y;
+	tangents[3*arrayPos+2] = tangent.z;
+	// color
+	colors[3*arrayPos] = col.x;
+	colors[3*arrayPos+1] = col.y;
+	colors[3*arrayPos+2] = col.z;
+	// uv coordinates
+	coords[2*arrayPos] = tangent.x;
+	coords[2*arrayPos+1] = tangent.y;
+}
+
+void MeshGrid::createUI(){
+
+}
+
+void MeshGrid::cleanup(){
+    if(normals!=NULL){
+        free(normals);
+        normals = NULL;
+    }
+    if(tangents!=NULL){
+        free(tangents);
+        tangents = NULL;
+    }
+    if(colors!=NULL){
+        free(colors);
+        colors = NULL;
+    }
+    if(vertices!=NULL){
+        free(vertices);
+        vertices = NULL;
+    }
+    if(faces!=NULL){
+        free(faces);
+        faces = NULL;
+    }
+    if(coords!=NULL){
+        free(coords);
+        coords = NULL;
+    }
+
+
+}
+
--- src/models/meshGrid.h
+++ src/models/meshGrid.h
@@ -19,15 +19,29 @@ class MeshGrid : public Mesh {
 
 
 public:
-	Grid(unsigned int size = 16, float width = 1, float z = 0);
-	~Grid();
+	MeshGrid(unsigned int size = 16, float w = 1.0f, float z = 0.0f);
+	~MeshGrid();
+
+	void createUI() override;
+	void recreate() override;
+
 private:
+	void createMesh(unsigned int size, float width, float z);
+	void addVertex(unsigned int arrayPos, glm::vec3 pos, glm::vec3 n, glm::vec3 tangent, glm::vec3 col, glm::vec2 uv);
+	void cleanup();
 
+	glm::vec3 defaultNormal = glm::vec3(0,0,1);
+	glm::vec3 defaultColor = glm::vec3(0,1,0);
+	glm::vec3 defaultTangent = glm::vec3(1,0,0);
 
+	unsigned int nbPointPerRowColumn = 0;
+	float width = 0.0f;
+	float gridZ = 0.0f;
 
 };
 
 
+	
 
 
 #endif
