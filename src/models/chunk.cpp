4ddaf870fa16fd71c48fdf112d564dea98013323
todorico <odoricothibault@gmail.com>
2019-11-30 18:32:36 +0100

    add generic chunk

--- src/models/chunk.cpp
+++ src/models/chunk.cpp
@@ -1,65 +1,90 @@
 #include "chunk.h"
+#include <iostream>
 
 
+///////////////////////// CubicGrid
 
-Chunk::Chunk(float size){
-    sizeOfOneCube = size;
-    createChunk();
+
+//// Accessors
+
+template<class T, size_t N>
+constexpr size_t CubicGrid<T, N>::width()  const {
+	return N;
+}
+
+template<class T, size_t N>
+constexpr size_t CubicGrid<T, N>::height() const {
+	return N;
+}
+
+template<class T, size_t N>
+constexpr size_t CubicGrid<T, N>::depth()  const {
+	return N;
 }
 
 
+template<class T, size_t N>
+T&       CubicGrid<T, N>::operator()(size_t x, size_t y, size_t z) {
+	return (*this)[x + y * this->width() + z * this->width() * this->height()];
+}
 
-Chunk::~Chunk(){
-
+template<class T, size_t N>
+const T& CubicGrid<T, N>::operator()(size_t x, size_t y, size_t z) const {
+	return (*this)[x + y * this->width() + z * this->width() * this->height()];
 }
 
 
-void Chunk::createChunk(){
-	for(unsigned int i=0; i<grid.size(); i++){
-		for(unsigned int j=0; j<grid[i].size(); j++){
-			for(unsigned int k=0; k<grid[i][j].size(); k++){
-				grid[i][j][k] = true;
+//// Booleans
+
+
+template<class T, size_t N>
+bool CubicGrid<T, N>::on_bounds(size_t x, size_t y, size_t z) const {
+	return x < this->width() && y < this->height() && z << this->depth();
+}
+
+
+///////////////////////// Chunk
+
+
+//// Constructors
+
+
+Chunk::Chunk(float voxelSize) : m_voxelSize(voxelSize) {
+	for (auto& v : (*this))
+		v = true;
+}
+
+
+//// Accessors
+
+
+float Chunk::voxelSize() const {
+	return m_voxelSize;
+}
+
+
+//// Drawing / OpenGL
+
+
+void Chunk::draw(glm::mat4 modelMat, glm::mat4 viewMat, glm::mat4 projectionMat, Light *light, Mesh *mesh, Material *material) {
+
+	for(unsigned int i = 0 ; i< this->width() ; i++) {
+		for(unsigned int j = 0 ; j < this->height() ; j++) {
+			for(unsigned int k = 0 ; k < this->depth() ; k++) {
+				if ((*this)(i, j, k))
+				{
+					glm::mat4 model = glm::translate(modelMat, glm::vec3(i * this->voxelSize(), j * this->voxelSize(), k * this->voxelSize()));
+			    	setUniform(model, viewMat, projectionMat, light, material);
+			    	mesh->drawVAO();
+				}
 			}
 		}
 	}
 }
 
-
-void Chunk::setUniform(glm::mat4 modelMat, glm::mat4 viewMat, glm::mat4 projectionMat, Light* light, Material *material){
+void Chunk::setUniform(glm::mat4 modelMat, glm::mat4 viewMat, glm::mat4 projectionMat, Light* light, Material *material) {
 
     // glm::mat4 model = transform->getMat4(modelMat);
-
-
-
     // send the transformation matrix
     material->callUniform(modelMat, viewMat, projectionMat, light);
-
-}
-
-
-
-
-void Chunk::draw(glm::mat4 modelMat, glm::mat4 viewMat, glm::mat4 projectionMat, Light *light, Mesh *mesh, Material *material){
-
-
-     for(unsigned int i=0; i<grid.size(); i++){
-		for(unsigned int j=0; j<grid[i].size(); j++){
-			for(unsigned int k=0; k<grid[i][j].size(); k++){
-
-				glm::mat4 model = glm::translate(modelMat, glm::vec3(i*sizeOfOneCube, j*sizeOfOneCube, k*sizeOfOneCube));
-
-			    setUniform(model, viewMat, projectionMat, light, material);
-
-			    mesh->drawVAO();
-
-			}
-		}
-
-	}
-
-
-}
-
-bool Chunk::getCell(unsigned int i, unsigned int j, unsigned int k){
-    return grid[i][j][k];
 }
