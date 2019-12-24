3d81cb1464f9c7a18f942df29ce6ab941847aa1a
Maxime <isnel.maxime@gmail.com>
2019-12-24 18:34:27 +0100

    ça semble marcher

--- src/models/mesh/meshIndexed.cpp
+++ src/models/mesh/meshIndexed.cpp
@@ -21,17 +21,17 @@ MeshIndexed::MeshIndexed(std::string filename) : m_filename(filename){
     m_object.vb_id = -1;
 
     createMeshFromFile(m_filename);
-    //createVAO();
+    createVAO();
 }
 
 
 MeshIndexed::~MeshIndexed(){
-
+    deleteVAO();
 }
 
 
 void MeshIndexed::createVAO(){
-    const int nbBuffer = 5;
+    const int nbBuffer = 4;
 
     m_buffers = new GLuint[nbBuffer];
 
@@ -45,14 +45,14 @@ void MeshIndexed::createVAO(){
     glBufferData(GL_ARRAY_BUFFER,getNBVertices()*3*sizeof(float),getVertices(),GL_STATIC_DRAW);
     glEnableVertexAttribArray(POSITION_ATTRIB);
     glVertexAttribPointer(POSITION_ATTRIB,3,GL_FLOAT,GL_FALSE,0,(void *)0);
-
+/*
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,m_buffers[1]); // indices
     glBufferData(GL_ELEMENT_ARRAY_BUFFER,getNBFaces()*3*sizeof(unsigned int),&m_indices[0],GL_STATIC_DRAW);
-
+*/
 
     // m_normals
     glEnableVertexAttribArray(VERTEX_NORMAL_ATTRIB);
-    glBindBuffer(GL_ARRAY_BUFFER, m_buffers[4]);
+    glBindBuffer(GL_ARRAY_BUFFER, m_buffers[1]);
     glBufferData(GL_ARRAY_BUFFER, getNBVertices()*3* sizeof(float), getNormals(), GL_STATIC_DRAW); //m_normals is std::vector<float>
     glVertexAttribPointer(VERTEX_NORMAL_ATTRIB, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
@@ -83,12 +83,15 @@ static void CheckErrors(std::string desc) {
 }
 
 void MeshIndexed::drawVAO(){
-    if(m_object.vb_id == -1){
+    glBindVertexArray(m_vertexArrayID);
+    glDrawArrays(GL_TRIANGLES,0,getNBVertices());
+    glBindVertexArray(0);
+    /*if(m_object.vb_id == -1){
         return;
-    }/*
+    }
     glBindVertexArray(m_vertexArrayID);
     glDrawElements(GL_TRIANGLES,getNBFaces(),GL_UNSIGNED_INT,(void *)0);
-    glBindVertexArray(0);*/
+    glBindVertexArray(0);
     GLsizei stride = (3 + 3 + 3 + 2) * sizeof(float);
 
     glBindBuffer(GL_ARRAY_BUFFER, m_object.vb_id);
@@ -97,13 +100,13 @@ void MeshIndexed::drawVAO(){
     glEnableClientState(GL_COLOR_ARRAY);
     glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 
-    /*glBindTexture(GL_TEXTURE_2D, 0);
+    glBindTexture(GL_TEXTURE_2D, 0);
     if ((o.material_id < materials.size())) {
       std::string diffuse_texname = materials[o.material_id].diffuse_texname;
       if (textures.find(diffuse_texname) != textures.end()) {
         glBindTexture(GL_TEXTURE_2D, textures[diffuse_texname]);
       }
-    }*/
+    }
     glVertexPointer(3, GL_FLOAT, stride, (const void*)0);
     glNormalPointer(GL_FLOAT, stride, (const void*)(sizeof(float) * 3));
     glColorPointer(3, GL_FLOAT, stride, (const void*)(sizeof(float) * 6));
@@ -111,12 +114,12 @@ void MeshIndexed::drawVAO(){
 
     glDrawArrays(GL_TRIANGLES, 0, 3 * m_object.numTriangles);
     CheckErrors("drawarrays");
-    glBindTexture(GL_TEXTURE_2D, 0);
+    glBindTexture(GL_TEXTURE_2D, 0);*/
   
 }
 
 void MeshIndexed::deleteVAO(){
-    glDeleteBuffers(5,m_buffers);
+    glDeleteBuffers(4,m_buffers);
     glDeleteVertexArrays(1,&m_vertexArrayID);
     delete m_buffers;
 }
@@ -384,7 +387,7 @@ static bool LoadObjAndConvert(float bmin[3], float bmax[3],
   }
 
   // Load diffuse textures
-  {
+  /*{
     for (size_t m = 0; m < materials.size(); m++) {
       tinyobj::material_t* mp = &materials[m];
 
@@ -435,7 +438,7 @@ static bool LoadObjAndConvert(float bmin[3], float bmax[3],
         }
       }
     }
-  }
+  }*/
 
   bmin[0] = bmin[1] = bmin[2] = std::numeric_limits<float>::max();
   bmax[0] = bmax[1] = bmax[2] = -std::numeric_limits<float>::max();
@@ -659,17 +662,308 @@ static bool LoadObjAndConvert(float bmin[3], float bmax[3],
 void MeshIndexed::createMeshFromFile(std::string filename){
 
 
-    std::vector<DrawObject> gDrawObjects;
-    float bmin[3], bmax[3];
-
+    tinyobj::attrib_t attrib;
+    std::vector<tinyobj::shape_t> shapes;
     std::vector<tinyobj::material_t> materials;
     std::map<std::string, GLuint> textures;
 
-    if (false == LoadObjAndConvert(bmin, bmax, &gDrawObjects, materials, textures,filename.c_str())) {
-        return;
+
+
+  std::string base_dir = GetBaseDir(filename);
+  if (base_dir.empty()) {
+    base_dir = ".";
+  }
+#ifdef _WIN32
+  base_dir += "\\";
+#else
+  base_dir += "/";
+#endif
+
+  std::string warn;
+  std::string err;
+
+   bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filename.c_str(),
+                              base_dir.c_str());
+  if (!warn.empty()) {
+    std::cout << "WARN: " << warn << std::endl;
+  }
+  if (!err.empty()) {
+    std::cerr << err << std::endl;
+  }
+
+
+  if (!ret) {
+    std::cerr << "Failed to load " << filename << std::endl;
+    return;
+  }
+
+
+  printf("# of vertices  = %d\n", (int)(attrib.vertices.size()) / 3);
+  printf("# of normals   = %d\n", (int)(attrib.normals.size()) / 3);
+  printf("# of texcoords = %d\n", (int)(attrib.texcoords.size()) / 2);
+  printf("# of materials = %d\n", (int)materials.size());
+  printf("# of shapes    = %d\n", (int)shapes.size());
+
+  // Append `default` material
+  materials.push_back(tinyobj::material_t());
+
+  for (size_t i = 0; i < materials.size(); i++) {
+    printf("material[%d].diffuse_texname = %s\n", int(i),
+           materials[i].diffuse_texname.c_str());
+  }
+
+  // Load diffuse textures
+  {
+    for (size_t m = 0; m < materials.size(); m++) {
+      tinyobj::material_t* mp = &materials[m];
+
+      if (mp->diffuse_texname.length() > 0) {
+        // Only load the texture if it is not already loaded
+        if (textures.find(mp->diffuse_texname) == textures.end()) {
+          GLuint texture_id;
+          int w, h;
+          int comp;
+
+          std::string texture_filename = mp->diffuse_texname;
+          if (!FileExists(texture_filename)) {
+            // Append base dir.
+            texture_filename = base_dir + mp->diffuse_texname;
+            if (!FileExists(texture_filename)) {
+              std::cerr << "Unable to find file: " << mp->diffuse_texname
+                        << std::endl;
+              exit(1);
+            }
+          }
+
+          unsigned char* image =
+              stbi_load(texture_filename.c_str(), &w, &h, &comp, STBI_default);
+          if (!image) {
+            std::cerr << "Unable to load texture: " << texture_filename
+                      << std::endl;
+            exit(1);
+          }
+          std::cout << "Loaded texture: " << texture_filename << ", w = " << w
+                    << ", h = " << h << ", comp = " << comp << std::endl;
+
+          glGenTextures(1, &texture_id);
+          glBindTexture(GL_TEXTURE_2D, texture_id);
+          glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+          glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+          if (comp == 3) {
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB,
+                         GL_UNSIGNED_BYTE, image);
+          } else if (comp == 4) {
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA,
+                         GL_UNSIGNED_BYTE, image);
+          } else {
+            assert(0);  // TODO
+          }
+          glBindTexture(GL_TEXTURE_2D, 0);
+          stbi_image_free(image);
+          textures.insert(std::make_pair(mp->diffuse_texname, texture_id));
+        }
+      }
     }
-    assert(gDrawObjects.size() > 0);
-    m_object = gDrawObjects[0];
+  }
+
+  m_minX = m_minY = m_minZ = std::numeric_limits<float>::max();
+  m_maxX = m_maxY = m_maxZ = -std::numeric_limits<float>::max();
+
+  
+    // std::vector<float> buffer;  // pos(3float), normal(3float), color(3float)
+
+    // Check for smoothing group and compute smoothing normals
+    /*std::map<int, vec3> smoothVertexNormals;
+    if (hasSmoothingGroup(shapes[s]) > 0) {
+    std::cout << "Compute smoothingNormal for shape [" << s << "]" << std::endl;
+    computeSmoothingNormals(attrib, shapes[s], smoothVertexNormals);
+    }*/
+
+
+    for (size_t f = 0; f < shapes[0].mesh.indices.size() / 3; f++) {
+        tinyobj::index_t idx0 = shapes[0].mesh.indices[3 * f + 0];
+        tinyobj::index_t idx1 = shapes[0].mesh.indices[3 * f + 1];
+        tinyobj::index_t idx2 = shapes[0].mesh.indices[3 * f + 2];
+
+
+        int current_material_id = shapes[0].mesh.material_ids[f];
+
+        float tc[3][2];
+        if (attrib.texcoords.size() > 0) {
+            if ((idx0.texcoord_index < 0) || (idx1.texcoord_index < 0) ||
+                (idx2.texcoord_index < 0)) {
+            // face does not contain valid uv index.
+            
+                tc[0][0] = 0.0f;
+                tc[0][1] = 0.0f;
+                tc[1][0] = 0.0f;
+                tc[1][1] = 0.0f;
+                tc[2][0] = 0.0f;
+                tc[2][1] = 0.0f;
+            } else {
+                assert(attrib.texcoords.size() >
+                        size_t(2 * idx0.texcoord_index + 1));
+                assert(attrib.texcoords.size() >
+                        size_t(2 * idx1.texcoord_index + 1));
+                assert(attrib.texcoords.size() >
+                        size_t(2 * idx2.texcoord_index + 1));
+
+                // Flip Y coord.
+                tc[0][0] = attrib.texcoords[2 * idx0.texcoord_index];
+                tc[0][1] = 1.0f - attrib.texcoords[2 * idx0.texcoord_index + 1];
+                tc[1][0] = attrib.texcoords[2 * idx1.texcoord_index];
+                tc[1][1] = 1.0f - attrib.texcoords[2 * idx1.texcoord_index + 1];
+                tc[2][0] = attrib.texcoords[2 * idx2.texcoord_index];
+                tc[2][1] = 1.0f - attrib.texcoords[2 * idx2.texcoord_index + 1];
+
+            
+            }
+
+        } else {
+            tc[0][0] = 0.0f;
+            tc[0][1] = 0.0f;
+            tc[1][0] = 0.0f;
+            tc[1][1] = 0.0f;
+            tc[2][0] = 0.0f;
+            tc[2][1] = 0.0f;
+        }
+
+        m_coords.push_back(glm::vec2(tc[0][0], tc[0][1]));
+        m_coords.push_back(glm::vec2(tc[1][0], tc[1][1]));
+        m_coords.push_back(glm::vec2(tc[2][0], tc[2][1]));
+
+        float v[3][3];
+        for (int k = 0; k < 3; k++) {
+            int f0 = idx0.vertex_index;
+            int f1 = idx1.vertex_index;
+            int f2 = idx2.vertex_index;
+            assert(f0 >= 0);
+            assert(f1 >= 0);
+            assert(f2 >= 0);
+
+            v[0][k] = attrib.vertices[3 * f0 + k];
+            v[1][k] = attrib.vertices[3 * f1 + k];
+            v[2][k] = attrib.vertices[3 * f2 + k];
+            /*bmin[k] = std::min(v[0][k], bmin[k]);
+            bmin[k] = std::min(v[1][k], bmin[k]);
+            bmin[k] = std::min(v[2][k], bmin[k]);
+            bmax[k] = std::max(v[0][k], bmax[k]);
+            bmax[k] = std::max(v[1][k], bmax[k]);
+            bmax[k] = std::max(v[2][k], bmax[k]);*/
+        }
+
+        m_vertices.push_back(glm::vec3(v[0][0], v[0][1], v[0][2]));
+        m_vertices.push_back(glm::vec3(v[1][0], v[1][1], v[1][2]));
+        m_vertices.push_back(glm::vec3(v[2][0], v[2][1], v[2][2]));
+
+        float n[3][3];
+        {
+            bool invalid_normal_index = false;
+            if (attrib.normals.size() > 0) {
+            int nf0 = idx0.normal_index;
+            int nf1 = idx1.normal_index;
+            int nf2 = idx2.normal_index;
+
+            if ((nf0 < 0) || (nf1 < 0) || (nf2 < 0)) {
+                // normal index is missing from this face.
+                invalid_normal_index = true;
+                std::cerr << "Invalid normal\n";
+            } else {
+                for (int k = 0; k < 3; k++) {
+                assert(size_t(3 * nf0 + k) < attrib.normals.size());
+                assert(size_t(3 * nf1 + k) < attrib.normals.size());
+                assert(size_t(3 * nf2 + k) < attrib.normals.size());
+                n[0][k] = attrib.normals[3 * nf0 + k];
+                n[1][k] = attrib.normals[3 * nf1 + k];
+                n[2][k] = attrib.normals[3 * nf2 + k];
+                }
+            }
+            } else {
+            invalid_normal_index = true;
+            }
+
+        
+
+            if (invalid_normal_index) {
+            // compute geometric normal
+            CalcNormal(n[0], v[0], v[1], v[2]);
+            n[1][0] = n[0][0];
+            n[1][1] = n[0][1];
+            n[1][2] = n[0][2];
+            n[2][0] = n[0][0];
+            n[2][1] = n[0][1];
+            n[2][2] = n[0][2];
+            }
+        }
+
+        m_vertices.push_back(glm::vec3(n[0][0], n[0][1], n[0][2]));
+        m_vertices.push_back(glm::vec3(n[1][0], n[1][1], n[1][2]));
+        m_vertices.push_back(glm::vec3(n[2][0], n[2][1], n[2][2]));
+
+        m_colors.push_back(glm::vec3(n[0][0], n[0][1], n[0][2]));
+        m_colors.push_back(glm::vec3(n[1][0], n[1][1], n[1][2]));
+        m_colors.push_back(glm::vec3(n[2][0], n[2][1], n[2][2]));
+
+        /*for (int k = 0; k < 3; k++) {
+            o.buffer.push_back(v[k][0]);
+            o.buffer.push_back(v[k][1]);
+            o.buffer.push_back(v[k][2]);
+            o.buffer.push_back(n[k][0]);
+            o.buffer.push_back(n[k][1]);
+            o.buffer.push_back(n[k][2]);
+            // Combine normal and diffuse to get color.
+            float normal_factor = 0.2;
+            float diffuse_factor = 1 - normal_factor;
+            float c[3] = {n[k][0] * normal_factor + diffuse[0] * diffuse_factor,
+                        n[k][1] * normal_factor + diffuse[1] * diffuse_factor,
+                        n[k][2] * normal_factor + diffuse[2] * diffuse_factor};
+            float len2 = c[0] * c[0] + c[1] * c[1] + c[2] * c[2];
+            if (len2 > 0.0f) {
+            float len = sqrtf(len2);
+
+                c[0] /= len;
+                c[1] /= len;
+                c[2] /= len;
+            }
+            o.buffer.push_back(c[0] * 0.5 + 0.5);
+            o.buffer.push_back(c[1] * 0.5 + 0.5);
+            o.buffer.push_back(c[2] * 0.5 + 0.5);
+
+            o.buffer.push_back(tc[k][0]);
+            o.buffer.push_back(tc[k][1]);
+        }*/
+    }
+
     
 
+    // OpenGL viewer does not support texturing with per-face material.
+    /*if (shapes[s].mesh.material_ids.size() > 0 &&
+        shapes[s].mesh.material_ids.size() > s) {
+    o.material_id = shapes[s].mesh.material_ids[0];  // use the material ID
+                                                        // of the first face.
+    } else {
+    o.material_id = materials.size() - 1;  // = ID for default material.
+
+    printf("shape[%d] material_id %d\n", int(s), int(o.material_id));*/
+
+    /*if (o.buffer.size() > 0) {
+    glGenBuffers(1, &o.vb_id);
+    glBindBuffer(GL_ARRAY_BUFFER, o.vb_id);
+    glBufferData(GL_ARRAY_BUFFER, o.buffer.size() * sizeof(float),
+                    &o.buffer.at(0), GL_STATIC_DRAW);
+    o.numTriangles = o.buffer.size() / (3 + 3 + 3 + 2) /
+                        3;  // 3:vtx, 3:normal, 3:col, 2:texcoord
+
+    printf("shape[%d] # of triangles = %d\n", static_cast<int>(s),
+            o.numTriangles);
+    }
+
+  
+  printf("bmin = %f, %f, %f\n", bmin[0], bmin[1], bmin[2]);
+  printf("bmax = %f, %f, %f\n", bmax[0], bmax[1], bmax[2]);*/
+
+  return;
+
+
 }
+
