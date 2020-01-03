cd680cbbef4c247f49c46fba478b32a4bde767b8
Maxime <isnel.maxime@gmail.com>
2020-01-03 18:41:35 +0100

    debut des projectiles

--- src/components/fireProjectiles.cpp
+++ src/components/fireProjectiles.cpp
@@ -26,14 +26,14 @@ void FireProjectiles::createProjectile(){
     Transform *transf = new Transform();
 
     glm::vec3 pos = m_gameobject->getTransform()->getPosition();
-    pos.z = 0.5f;
+    pos.z += 0.5f;
     transf->setPosition(pos);
     transf->setRotation(m_gameobject->getTransform()->getRotation());
 
     GameObject *projectile = new GameObject(m_scene->addNewId(), "Projectile", transf);
     projectile->addComponent<MeshRenderer*>(new MeshRenderer());
-    // projectile->addComponent<Mesh*>(new MeshCube(0.5f));
-    // projectile->addComponent<Material*>(new Lambertian());
+    projectile->addComponent<Mesh*>(new MeshCube(0.5f));
+    projectile->addComponent<Material*>(new Lambertian());
 
     m_scene->addGameObject(projectile);
 
