7cea2a04cd15f0439f936be0685c026a81d834a7
Maxime <isnel.maxime@gmail.com>
2020-01-12 11:41:51 +0100

    test fix compile windows

--- src/components/cameraFollow.cpp
+++ src/components/cameraFollow.cpp
@@ -1,5 +1,13 @@
 #include <imgui.h>
 
+#ifndef GLM_H
+#define GLM_H
+#include <glm/gtx/perpendicular.hpp>
+#include <glm/glm.hpp>
+#include <glm/ext/matrix_transform.hpp>
+#include <glm/ext/matrix_clip_space.hpp>
+#include <glm/gtc/matrix_inverse.hpp>
+#endif
 
 #include "cameraFollow.h"
 
