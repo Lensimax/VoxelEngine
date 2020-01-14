78f7a26876088856606317cf813999a7fe1c664d
Maxime <isnel.maxime@gmail.com>
2020-01-14 12:16:01 +0100

    ajout de la demo 3D

---
+++ demo3D/CMakeFiles/voxel-engine.dir/DependInfo.cmake
@@ -0,0 +1,70 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/thespygeek/Projet/voxelEngine/src/components/axisRenderer.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/components/axisRenderer.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/components/cameraControllerFirstPerson.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/components/cameraControllerFirstPerson.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/components/cameraFollow.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/components/cameraFollow.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/components/cameraProjective.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/components/cameraProjective.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/components/cameraRenderer.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/components/cameraRenderer.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/components/colliders/collider.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/components/colliders/collider.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/components/component.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/components/component.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/components/debug/debugTransform.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/components/debug/debugTransform.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/components/fireProjectiles.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/components/fireProjectiles.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/components/groundFollow.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/components/groundFollow.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/components/meshRenderer.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/components/meshRenderer.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/components/playerController.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/components/playerController.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/components/projectile.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/components/projectile.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/components/rigidbody.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/components/rigidbody.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/components/terrainModificator.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/components/terrainModificator.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/components/thirdPersonController.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/components/thirdPersonController.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/engineClass/InputManager.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/engineClass/InputManager.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/engineClass/UI.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/engineClass/UI.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/engineClass/gameObject.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/engineClass/gameObject.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/engineClass/mainRenderer.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/engineClass/mainRenderer.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/engineClass/scene.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/engineClass/scene.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/main.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/main.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/material/lambertian.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/material/lambertian.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/material/material.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/material/material.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/material/shader.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/material/shader.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/material/simpleMat.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/material/simpleMat.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/material/textureMaterial.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/material/textureMaterial.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/models/mesh/mesh.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/models/mesh/mesh.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/models/mesh/meshCube.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/models/mesh/meshCube.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/models/transform.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/models/transform.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/terrain/simplexNoise.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/terrain/simplexNoise.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/terrain/terrainChunk.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/terrain/terrainChunk.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/terrain/terrainManager.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/terrain/terrainManager.cpp.o"
+  "/home/thespygeek/Projet/voxelEngine/src/tools/lights/directionnalLight.cpp" "/home/thespygeek/Projet/voxelEngine/build/CMakeFiles/voxel-engine.dir/src/tools/lights/directionnalLight.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLFW_INCLUDE_NONE"
+  "IMGUI_IMPL_OPENGL_LOADER_GLAD"
+  "NDEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../thirdparty/glm"
+  "../thirdparty/glfw/include"
+  "../thirdparty/glad/include"
+  "../thirdparty/imgui"
+  "../thirdparty/stb_image"
+  "../thirdparty/utilities"
+  "../thirdparty/glm/glm/.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/thespygeek/Projet/voxelEngine/build/thirdparty/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/home/thespygeek/Projet/voxelEngine/build/thirdparty/CMakeFiles/glad.dir/DependInfo.cmake"
+  "/home/thespygeek/Projet/voxelEngine/build/thirdparty/imgui/CMakeFiles/imgui.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
