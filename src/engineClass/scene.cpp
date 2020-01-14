867b09f3b4d407c63643d66461e6c3162bb36434
Maxime <isnel.maxime@gmail.com>
2020-01-14 12:08:42 +0100

    build de la demo de charge de chunk

--- src/engineClass/scene.cpp
+++ src/engineClass/scene.cpp
@@ -47,8 +47,8 @@
 
 Scene::Scene(){
     // loadGameplayScene();
-    loadSampleScene();
-    // loadExplorationScene();
+    // loadSampleScene();
+    loadExplorationScene();
 }
 
 Scene::~Scene(){
@@ -279,7 +279,7 @@ void Scene::loadExplorationScene(){
     objectsEngine.push_back(player);
 
     GameObject *terrain = new GameObject(addNewId(), "Terrain");
-    terrain->addComponent<TerrainManager*>(new TerrainManager(32, 30, player->getTransform()));
+    terrain->addComponent<TerrainManager*>(new TerrainManager(32, 15, player->getTransform()));
     objectsEngine.push_back(terrain);
   
 
